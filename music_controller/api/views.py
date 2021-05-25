8d9bce8efb535b0bd9ebb2e47a2c462344d0ae67
Augusto Peres <augustoapg@gmail.com>
2021-05-25 16:18:23 -0400

    add basic FE and api call to create room

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,10 +1,47 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
 from .models import Room
-from .serializers import RoomSerializer
+from .serializers import RoomSerializer, CreateRoomSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+class CreateRoomView(APIView):
+    serializer_class = CreateRoomSerializer
+
+    def post(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        # send the data to serializer, which will give us a python representation of the data we can use
+        serializer = self.serializer_class(data=request.data)
+
+        # if guest_can_pause and votes_to_skip are in the data and if they are valid
+        if serializer.is_valid():
+            guest_can_pause = serializer.data.get('guest_can_pause')
+            votes_to_skip = serializer.data.get('votes_to_skip')
+            host = self.request.session.session_key
+            
+            # check if user already has an active room. If they do, just update variables
+            queryset = Room.objects.filter(host=host)
+
+            if queryset.exists():
+                room = queryset[0]
+                room.guest_can_pause = guest_can_pause
+                room.votes_to_skip = votes_to_skip
+                room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+                return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
+            else:
+                room = Room(host=host, guest_can_pause=guest_can_pause, votes_to_skip=votes_to_skip)
+                room.save()
+            
+            # calls the serializer to transform the room object into json to return
+            return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
+        
+        return Response({'Bad Request': 'Invalid data'}, status=status.HTTP_400_BAD_REQUEST)
+
